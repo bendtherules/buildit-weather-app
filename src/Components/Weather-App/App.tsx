5b64a6f730a682a466a3d71e58faab41dfb3ba8b
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2018-03-27 17:34:14 +0530

    Add  data api fetching

--- src/Components/Weather-App/App.tsx
+++ src/Components/Weather-App/App.tsx
@@ -3,7 +3,56 @@ import './App.css';
 
 const logo = require('./logo.svg');
 
+export interface ForecaseAtInstance {
+  dt: Number;
+  main: {
+    temp: Number;
+    temp_min: Number;
+    temp_max: Number;
+    pressure: Number;
+    sea_level: Number;
+    grnd_level: Number;
+    humidity: Number;
+    temp_kf: Number;
+  };
+
+  weather: [{
+    id: Number;
+    main: string; // Make enum
+    description: string;
+    icon: string;
+  }];
+
+  clouds: {
+    all: Number;
+  };
+
+  wind: {
+    speed: Number;
+    deg: Number;
+  };
+
+  dt_txt: string;
+}
+
+export interface Forecast5Days {
+  cnt: Number;
+  list: Array<ForecaseAtInstance>;
+}
+
 export class WeatherApp extends React.Component {
+  data: Forecast5Days;
+
+  componentDidMount() {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=kolkata&appid=d65cee3c5866168080fa1c4177fdb4a8')
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseObject) => {
+        this.data = responseObject as Forecast5Days;
+      });
+  }
+
   render() {
     return (
       <div className="App">
